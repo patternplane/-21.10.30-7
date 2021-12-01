1b82d922a9b6a17a726f52ec382eaee6366195b6
patternplane <bss011202@naver.com>
2021-12-02 01:20:17 +0900

    [과제11][INIT] 과제11 준비 및 생성

---
+++ 실습11/21.11.23 과제11.cpp
@@ -0,0 +1,34 @@
+
+/**
+* @author 백성수
+* @studentId
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// ■ 헤더
+
+// ● 그래프 생성
+
+/**
+* 사용자 입력을 받아 그래프를 생성합니다.
+* 
+* @param node_count : 그래프의 노드 수
+* @return 생성된 그래프를 표현한 인접 행렬 배열
+*/
+int** make_graph(int node_count);
+
+// ■ main
+
+int main() {
+
+	printf("■ 이상 프로그램을 종료합니다.\n진행하려면 아무 숫자나 입력 : ");
+	int final_exit_answer;
+	scanf_s("%d", &final_exit_answer);
+
+	return 0;
+}
+
+// ■ 구현부
