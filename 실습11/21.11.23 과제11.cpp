4c71a1cbf74a3230bcc3bc7fca709435327b6ca5
patternplane <bss011202@naver.com>
2021-12-02 04:23:44 +0900

    [과제11][FEAT/TEST/FIX] 함수가 구현되어 테스트 및 수정됨

--- 실습11/21.11.23 과제11.cpp
+++ 실습11/21.11.23 과제11.cpp
@@ -10,6 +10,19 @@
 
 // ■ 헤더
 
+// ● 상수
+
+// bool
+#define TRUE 1
+#define FALSE 0
+
+// 그래프의 가중치 최대값
+#define COST_MAX 100000
+
+// ● 동적할당
+
+void* malloc_s(size_t size);
+
 // ● 그래프 생성
 
 /**
@@ -55,6 +68,37 @@ int** allCosts(int** cost, int n);
 
 int main() {
 
+	int n, start_v;
+	printf("■ 단일 출발점에서 최단 경로를 찾습니다.\n");
+	printf("노드 수 : ");
+	scanf_s("%d",&n);
+	int** graph = make_graph(n);
+	printf("■ 시작 노드 : ");
+	scanf_s("%d",&start_v);
+	int* distance = shortestPath(start_v, graph, n);
+	for (int i = 0; i < n; i++) {
+		if (distance[i] == COST_MAX)
+			printf("∞ ");
+		else
+			printf("%d ", distance[i]);
+	}
+	printf("\n\n");
+
+	printf("■ 모든 출발점에 대해 최단 경로를 찾습니다.\n");
+	int** distance_all = allCosts(graph, n);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (distance_all[i][j] == COST_MAX)
+				printf("∞ ");
+			else
+				printf("%d ", distance_all[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n\n");
+
+	// ■ 동적할당 해제하기
+
 	printf("■ 이상 프로그램을 종료합니다.\n진행하려면 아무 숫자나 입력 : ");
 	int final_exit_answer;
 	scanf_s("%d", &final_exit_answer);
@@ -63,3 +107,93 @@ int main() {
 }
 
 // ■ 구현부
+
+void* malloc_s(size_t size) {
+	void* tmp = malloc(size);
+	if (!tmp) {
+		fprintf(stderr,"Out Of Memory\n");
+		exit(1);
+	}
+	return tmp;
+}
+
+int** make_graph(int node_count) {
+	// 그래프를 생성하여 초기화
+	int** graph = (int**)malloc_s(node_count*sizeof(int*));
+	for (int i = 0; i < node_count; i++) {
+		graph[i] = (int*)malloc_s(node_count * sizeof(int));
+		for (int j = 0; j < node_count; j++)
+			graph[i][j] = ((i == j)?0:COST_MAX); // 자기 자신에게 돌아가는 cost는 일반적으로 0, 그 외에는 아직 간선이 없으므로 ∞로 설정
+	}
+
+	// 간선 정보를 입력받음
+	int start, end, cost;
+	printf("■ (시작 정점, 종료 정점, 가중치)를 입력하세요.\n종료하려면 -1 -1 -1을 입력.\n");
+	while (true) {
+		scanf_s("%d %d %d",&start, &end, &cost);
+
+		if (start == -1 || end == -1 || cost == -1) // 종료상황
+			break;
+		if (start < 0 || start >= node_count || end < 0 || end >= node_count) { // 잘못된 정점 기입
+			printf("정점 번호가 잘못되었습니다, 확인해주세요.\n");
+			continue;
+		}
+		if (cost < 0) { // 잘못된 가중치 기입
+			printf("가중치 값이 잘못되었습니다, 확인해주세요.\n");
+			continue;
+		}
+
+		graph[start][end] = cost;
+	}
+
+	return graph;
+}
+
+int* shortestPath(int v, int** cost, int n) {
+	int* distance = (int*)malloc_s(n*sizeof(int));
+	short* found = (short*)malloc_s(n * sizeof(short));
+	for (int i = 0; i < n; i++){
+		distance[i] = cost[v][i];
+		found[i] = FALSE;
+	}
+	found[v] = TRUE;
+	int u;
+	for (int i = 0; i < n - 2; i++) {
+		if ((u = choose(found, distance, n)) == -1)
+			break;
+		found[u] = TRUE;
+		for (int w = 0; w < n; w++)
+			if (!found[w])
+				if (distance[u] + cost[u][w] < distance[w])
+					distance[w] = distance[u] + cost[u][w];
+	}
+	return distance;
+}
+
+int choose(short* found, int* distance, int n) {
+	int min_i = -1;
+	int min = COST_MAX;
+	for (int i = 0; i < n; i++)
+		if (!found[i] && distance[i] < min) {
+			min_i = i;
+			min = distance[i];
+		}
+	return min_i;
+}
+
+int** allCosts(int** cost, int n) {
+	int** distance = (int**)malloc_s(n * sizeof(int*));
+	for (int i = 0; i < n; i++) {
+		distance[i] = (int*)malloc_s(n * sizeof(int));
+		for (int j = 0; j < n; j++)
+			distance[i][j] = cost[i][j];
+	}
+
+	for (int k = 0; k < n; k++)
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				if (distance[i][k] + distance[k][j] < distance[i][j])
+					distance[i][j] = distance[i][k] + distance[k][j];
+
+	return distance;
+}
