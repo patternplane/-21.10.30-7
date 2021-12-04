4ce1ba2871fb1c8c0602c5bb80c6eea18e270bbf
patternplane <bss011202@naver.com>
2021-12-04 09:47:25 +0900

    [과제12][FIX] test2 정렬 선택 인덱스 오류 수정

--- 실습12/21.12.3 과제12.cpp
+++ 실습12/21.12.3 과제12.cpp
@@ -338,12 +338,12 @@ void test2() {
 			for (int k = 0; k < testCase_len; k++) {
 				arrayCopy(j, arrayB, arrayA, testCase[k]);
 				start = clock();
-				(sortAlgorithm[j])(arrayB, testCase[k]);
+				(sortAlgorithm[i])(arrayB, testCase[k]);
 				end = clock();
 				if (sortCheck(arrayB, testCase[k]))
-					printf("%s 정렬 | %7d개 : %0.3f초\n", sortName[j], testCase[k], (end - start) / (double)CLOCKS_PER_SEC);
+					printf("%s 정렬 | %7d개 : %0.3f초\n", sortName[i], testCase[k], (end - start) / (double)CLOCKS_PER_SEC);
 				else
-					printf("%s정렬 오류! 정렬되지 않았습니다.\n", sortName[k]);
+					printf("%s정렬 오류! 정렬되지 않았습니다.\n", sortName[i]);
 			}
 		}
 		printf("\n");
